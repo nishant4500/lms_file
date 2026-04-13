016a3df45b56a5b716f767892160996b09100e7a
Nishant Dwivedi <135944619+nishant4500@users.noreply.github.com>
2026-04-13 10:41:03 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,9 +15,14 @@ from app.routes import courses
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://ambitious-glacier-0d2d4010f.7.azurestaticapps.net/",  # ✅ your Azure frontend
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
